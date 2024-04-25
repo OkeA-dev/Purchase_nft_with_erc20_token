2637f983b7a0a4d7026a9ba2594b150f7c91f50a
Oke.A <Okea.dev@gmail.com>
2024-04-25 23:23:34 +0100

    ...

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MyNFT is ERC721URIStorage, Ownable {
     uint256 private _tokenIdCounter;
 
-    constructor() ERC721("MyNFT", "MNFT") {}
+    constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {}
 
     function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
         _tokenIdCounter++;
--- ignition/modules/MyNft.js
+++ ignition/modules/MyNft.js
@@ -1,63 +1,25 @@
-const fs = require('fs');
-const solc = require('solc');
-const Web3 = require('web3');
 const { buildModule } = require('@nomicfoundation/hardhat-ignition/modules');
+const fs = require('fs');
 
-//Read the smart contract code
-const contractSource = fs.readFileSync('MyNft.sol', 'utf8');
+async function main() {
+  const contractSource = fs.readFileSync('contracts/MyNFT.sol', 'utf8');
 
-// Compile the contract
-const input = {
-    language: 'Solidity',
-    sources: {
-        'MyNFT.sol': {
-            content: contractSource
-        }
-    },
-    settings: {
-        outputSelection: {
-            '*': {
-                '*': ['*']
-            }
-        }
-    }
-};
+  const contractModule = {
+    id: 'MyNFTModule',
+    source: contractSource,
+    solcVersion: '0.8.24',
+  };
+  
+  //Build the contract modules
+  const contract = await buildModule(contractModule);
 
-const tempFile = JSON.parse(solc.compile(JSON.stringify(input)));
-const contractFile = tempFile.contracts['MyNft.sol']['MyNFT'];
+  const deployedContract = await contract.deploy({ args: [] });
+  console.log('Contract deployed at:', deployedContract.address);
+}
 
-//Create an instance of the Web3 library
-const web3 = new Web3(`https://eth-sepolia.g.alchemy.com/v2/${process.env.SEPOLIA_URL}`);
-
-// Set up the contract deployment parameter
-const bytecode = '0x' + contractFile.evm.bytecode.object;
-const abi = contractFile.abi;
-
-//Build the deployment module
-const deployModule = buildModule({
-    bytecode: bytecode,
-    abi: abi,
-    gas: 4700000,
-    gasPrice: 100000000000
-});
-
-//Deploy the contract
-web3.eth.sendTransaction({
-    from: '0xa324CBeb48D0Dede644eD671D45e78873Ae8127e',
-    data: deployModule.data
-})
-.once('transtionHash', (txHash) => {
-    console.log('Transaction hash:', txHash);
-})
-.once('recipient', (receipt) => {
-    console.log('Contract deployed at address:', receipt.contractAddress);
-
-    //Get the deployed contract instance
-    const deployedContract = new web3.eth.Contract(abi, receipt.contractAddress);
-
-    console.log('Deployed contract instance:', deployedContract);
-
-})
-.on('error', (error) => {
-    console.error('Error deploying contract:', error);
-})
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
